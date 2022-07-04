e282fed33eb6de1e7b6355e05f1406a06a97b399
Gabriela Ramires <engenheira.ramires@gmail.com>
2022-07-04 17:44:03 -0300

    Primeira Versão

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import './styles.css'
+import api from './services/api.js'
 
 function App() {
+
+ 
+
+
+const [ input, setInput ] = useState('') //input é o valor do estado, setInput é para passar o novo valor do estado
+
+const [poke, setPoke] = useState({});
+
+async function Pesquisar(){ //função assincrona
+  if (input === ""){
+    alert("Preencha algum nome")
+    return;
+  }
+
+  try{ //o que quer fazer, mas pode dar errado
+      const response = await api.get(`${input}`)
+      setPoke(response.data)
+      setInput("")// deixa o input vazio
+  }
+
+  catch{
+    alert("Ops, não foi possível encontrar o Pokémon.")
+    setInput("")
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Pokémon</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite um nome de pokémon"
+        value={input}
+        onChange={(evento) => setInput(evento.target.value) } // evento.target.value é o valor do input que está sendo mandado para o setInput
+        />
+
+        <div className="botao">
+        <button className="buttonSearch" onClick={Pesquisar}>
+          <FiSearch size={25} color="#FFF"/>
+        </button>
+        </div>
     </div>
+
+        {Object.keys(poke).length > 0 && (
+           <main className="main">
+          <h2>{poke.name}</h2>
+          
+          <img src={poke.sprites.other.dream_world.front_default}/>
+          <span><strong>Tipo: </strong>{poke.types.map(typeInfo => typeInfo.type.name)}</span>
+          <span><strong>Habilidades: </strong>{poke.abilities.map(typeInfo => typeInfo.ability.name).join(' | ')}</span>
+
+        </main>
+        )
+        
+        }
+
+       
+
+    
+<div className="footer">Desenvolvido por: Gabriela Ramires | Para mais aplicações: <a href="https://portfolio-gabi.herokuapp.com/">www.portfolio-gabi.herokuapp.com</a></div>
+      
+    </div>
+    
   );
 }
 
